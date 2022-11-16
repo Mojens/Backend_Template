af88ae34495cc562f963a3bdd3b66a9dfab74dea
Mojens <moha135h@stud.kea.dk>
2022-11-16 09:39:54 +0100

    controller

--- src/main/java/com/example/template/template/dto/CarResponse.java
+++ src/main/java/com/example/template/template/dto/CarResponse.java
@@ -1,6 +1,7 @@
 package com.example.template.template.dto;
 
 import com.example.template.template.entity.Car;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CarResponse {
 
   private int id;

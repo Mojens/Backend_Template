c3f277f66458a02f92fbc22241c993edd7bf6eb0
Mojens <moha135h@stud.kea.dk>
2022-11-18 21:28:32 +0100

    Implement Login

---
+++ src/main/java/com/example/template/security/entity/Role.java
@@ -0,0 +1,9 @@
+package com.example.template.security.entity;
+
+/*
+Add required roles for your project to this enum
+ */
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/example/template/template/api/CarController.java
+++ src/main/java/com/example/template/template/api/CarController.java
@@ -4,6 +4,7 @@ import com.example.template.template.dto.CarResponse;
 import com.example.template.template.service.CarService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,16 +19,19 @@ public class CarController {
     this.carService = carService;
   }
 
+  @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
   @GetMapping
   List<CarResponse> getAllCars(Pageable p) {
     return carService.getAllCars(p);
   }
 
+  @PreAuthorize("hasAuthority('ADMIN')")
   @GetMapping("/all")
   List<CarResponse> getAllCars() {
     return carService.getAllCars();
   }
 
+  @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
   @GetMapping("/filter")
   List<CarResponse> getAllCars(@RequestParam("column") String column, @RequestParam("value") String value, Pageable p) {
     return carService.filter(column,value,p);
